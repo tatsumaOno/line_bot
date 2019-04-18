f9633344909237d759e1d6e050ef269bf856c801
tatsumaOno <onotatsuma@onodarumanoMacBook-puro.local>
2019-04-19 01:16:31 +0900

    edit

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -35,7 +35,7 @@ class LinebotController < ApplicationController
       hash_result = JSON.parse result
       shops = hash_result["rest"]
       puts shops
-      shop = shops{0}
+      shop = shops[0]
       url = shop["url_mobile"]
       shop_name = shop["name"]
       category = shop["category"]
